30c4a21c22d0cecba314554240ee41c6d9f1bc86
santoso21 <30901764+santoso21@users.noreply.github.com>
2018-07-14 23:00:46 +0700

    Add files via upload

--- web_theme_lte/__manifest__.py
+++ web_theme_lte/__manifest__.py
@@ -1,16 +1,14 @@
-9# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 {
-    "name": "AdminLTE Backend Theme AdminLTE",
+    "name": "Web Theme AdminLTE",
     "summary": "It provides a mobile theme like adminLTE Bootstrap compliant interface for Odoo Community "
                "web",
     "version": "10.0.0.0.1",
     "category": "Theme",
     "website": "",
     "author": "Santoso SA.",
-    "license": "OPL-1",
-    "price": "99.99",
-    "currency": "EUR",
+    "license": "LGPL-3",
     "installable": True,
     "depends": [
         'web',        
@@ -23,5 +21,4 @@
         'static/src/xml/form_view.xml',
         'static/src/xml/base.xml',        
     ],
-    'images': ['images/main_screenshot.png','images/main_1.png','images/main_2.png']
 }
