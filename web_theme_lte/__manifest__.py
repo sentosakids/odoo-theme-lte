5059006ed3ee06d1a6151fff17df8cc2e4db53d4
santoso21 <30901764+santoso21@users.noreply.github.com>
2018-08-25 16:52:32 +0700

    Add files via upload

--- web_theme_lte/__manifest__.py
+++ web_theme_lte/__manifest__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
 {
-    "name": "Backend Theme AdminLTE",
+    "name": "Backend Theme AdminLTE 2",
     "summary": "It provides fully responsive mobile adminLTE Bootstrap for Backend Theme Odoo Community "
                "web",
-    "version": "10.0.0.0.1",
+    "version": "10.0.0.0.2",
     "category": "Theme",
     "price":124,
     "currency": 'EUR',
     "website": "https://santoso21.000webhostapp.com/",
-    #"live_test_url": "https://santoso21.000webhostapp.com/odoo11",
+    "live_test_url": "https://santoso21.000webhostapp.com/odoo11",
     "author": "Santoso SA.",
     "license": "OPL-1",
     "installable": True,
@@ -24,5 +24,5 @@
         'static/src/xml/form_view.xml',
         'static/src/xml/base.xml',        
     ],
-    'images': ['images/main_screenshot.png', 'images/main_1.png']
+    #'images': ['images/main_screenshot.png', 'images/main_1.png']
 }
