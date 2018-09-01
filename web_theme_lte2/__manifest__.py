24db8910ec7dde55338ee78c3f62ef7a38cf73f4
santoso21 <30901764+santoso21@users.noreply.github.com>
2018-09-01 20:30:11 +0700

    Add files via upload

---
+++ web_theme_lte2/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Backend Theme AdminLTE 2",
+    "summary": "It provides fully responsive mobile adminLTE Bootstrap for Backend Theme Odoo Community "
+               "web",
+    "version": "10.0.0.0.2",
+    "category": "Theme",
+    "price":124,
+    "currency": 'EUR',
+    "website": "https://santoso21.000webhostapp.com/odoo10",
+    "live_test_url": "https://santoso21.000webhostapp.com/odoo10",
+    "author": "Santoso SA.",
+    "license": "OPL-1",
+    "installable": True,
+    "depends": [
+        'web',        
+    ],
+    "data": [
+        'views/assets.xml',
+        'views/web.xml',
+    ],
+    'qweb': [
+        'static/src/xml/form_view.xml',
+        'static/src/xml/base.xml',        
+    ],
+    'images': ['images/main_screenshot.png', 'images/main_1.png']
+}
---
+++ web_theme_lte2/doc/index.rst
@@ -0,0 +1,23 @@
+================
+Installation
+================
+ - Just extract the module
+ - add to your addons folder
+ - then go to your Odoo Apps
+ - If you can't see the module, click "Update Apps List", wait until process done then search again
+ - After you found the module , just click Install
+ - Done.
+ 
+ Now you got AdminLTE theme for your odoo application.
+
+===============
+Implementation
+===============
+For now company name not automatic changed, so you can change manually from xml :
+- go to your module theme_lte > views > web.xml
+- search for "My Company"
+- then change with your company name
+
+I hope in another version it would be automatically changed and many features.
+
+Enjoy the theme.
